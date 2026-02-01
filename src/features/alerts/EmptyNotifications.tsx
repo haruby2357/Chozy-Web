daaec74cd9f1ca8624fac7d03cc753af79186aac
haruby2357 <haruby2357@gmail.com>
2026-02-01 09:00:11 +0900

    feat : 알림 기본 창 디자인 변경

--- src/features/alerts/EmptyNotifications.tsx
+++ src/features/alerts/EmptyNotifications.tsx
@@ -1,12 +1,10 @@
 export default function EmptyNotifications() {
   return (
     <main className="flex-1 flex items-center justify-center bg-white px-4 text-center">
-      <div className="flex flex-col items-center gap-2 -translate-y-10">
+      <div className="flex flex-col items-center gap-10 -translate-y-10">
+        <div className="w-[100px] h-[100px] bg-[#D9D9D9]" />
         <p className="text-[#B5B5B5] text-[16px] font-medium leading-[24px]">
           새로운 알림이 없어요.
-        </p>
-        <p className="text-[#B5B5B5] text-[16px] font-medium leading-[24px]">
-          화면을 둘러볼까요?
         </p>
       </div>
     </main>
