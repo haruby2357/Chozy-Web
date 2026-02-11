40c65fe3d5abb02b8935bd3ecafb5a0e97172a35
신보연 <eungehby@konkuk.ac.kr>
2026-02-11 15:29:56 +0900

    [feat] 로그인 - 약관 동의 상세페이지 구현

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../features/login/Login";
 import Terms from "../features/login/Terms";
+import ServiceTerms from "../features/login/ServiceTerms";
+import PrivacyPolicy from "../features/login/PrivacyPolicy";
 import Home from "../features/goodsPage/Home";
 import SearchEntry from "../features/goodsPage/SearchEntry";
 import SearchResult from "../features/goodsPage/SearchResult";
@@ -20,6 +22,8 @@ export default function AppRouter() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/login/terms" element={<Terms />} />
+      <Route path="/login/terms/service" element={<ServiceTerms />} />
+      <Route path="/login/terms/privacy" element={<PrivacyPolicy />} />
       <Route path="/" element={<Home />} />
 
       {/* 상품 검색 페이지 라우팅 */}
